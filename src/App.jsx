3f881277be9cccceebb25fc04e7ae2b9cd5ff3d7
Arsallan Shahab <mdarsallan@gmail.com>
2022-09-17 20:30:09 +0530

    boilerplate code

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center text-3xl">
+      React + ViteJs + TailwindCss = :) Lit
+    </div>
+  );
+}
+
+export default App;
